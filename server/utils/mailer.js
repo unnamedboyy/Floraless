85850ea2095e24cedeff38456122fd219ca1f5dc
unnamedboyy <kaisarsimaa22@gmail.com>
2026-06-23 21:43:59 +0700

    modified:   server/utils/mailer.js

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -6,8 +6,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || "smtp.gmail.com",
-  port: Number(process.env.EMAIL_PORT) || 465,
-  secure: true,
+  port: 587,
+  secure: false,
   requireTLS: true,
 
   auth: {
